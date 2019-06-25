9f059310faf7b04262ce2d0f3ef828db80018231
elviswong <elviswong@aliyun.com>
2019-06-25 18:29:07 +0800

    update module name

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,23 +1,12 @@
 import 'package:flutter/material.dart'; // Android MD UI 设计风格
 import 'package:startup_namer/routes/EchoRoute.dart';
 import 'package:startup_namer/routes/MyRoute.dart';
-import 'package:startup_namer/widgets/stateful/ConstrainedWidget.dart';
-import 'package:startup_namer/widgets/stateful/FlexWidget.dart';
-import 'package:startup_namer/widgets/stateful/FormWidget.dart';
-import 'package:startup_namer/widgets/stateful/StackAndPositionWidget.dart';
-import 'package:startup_namer/widgets/stateful/WrapFlowWidget.dart';
-import 'package:startup_namer/widgets/stateless/ButtonWidget.dart';
 import 'package:startup_namer/widgets/stateful/CouterWidgetState.dart';
 import 'package:startup_namer/widgets/stateful/HomeWidget.dart';
-import 'package:startup_namer/widgets/stateful/RowAndColumnWidget.dart';
-import 'package:startup_namer/widgets/stateless/FocusWidget.dart';
-import 'package:startup_namer/widgets/stateless/PaddingWidget.dart';
-import 'package:startup_namer/widgets/stateless/SwitchAndCheckboxWidget.dart';
-import 'package:startup_namer/widgets/stateless/TextFieldWidget.dart';
-import 'package:startup_namer/widgets/stateless/TextWidget.dart';
 
 // Dart语言的运行入口main函数
 void main(List<String> args) => runApp(MyApp());
+
 
 // 主界面
 class MyApp extends StatelessWidget {
@@ -30,34 +19,23 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blue,
       ),
       routes: {
-        'my_page': (context) => MyRoute(),
-        'echo_route': (context) => EchoRoute(),
-        'couter_widget_state': (context) => CouterWidgetState(initValue: 0),
-        'row_column': (context) => RowAndColumnWidget(),
-        'text_widgetful': (context) => TextWidgetful(),
-        'button_widgetful': (context) => ButtonWidget(),
-        'switchAndCb_widget': (context) => SwitchAndCheckboxWidget(),
-        'textField_widget': (context) => TextFieldWidget(),
-        'focus_widget': (context) => FocusWidget(),
-        'form_widget': (context) => FormWidget(),
-        'flex_widget': (context) => FlexWidget(),
-        'wrapFlow_widget': (context) => WrapFlowWidget(),
-        'stackPositioned_widget': (context) => StackAndPositionWidget(),
-        'padding_widget': (context) => PaddingWidget(),
-        'constrained_widget': (context) => ConstrainedWidget(),
+        'my_page':(context)=>new MyRoute(),
+        'echo_route':(context)=>new EchoRoute(),
+        // 'couter_widget_state':(context)=>new CouterWidgetState()
       },
       home: Scaffold(
         //  appBar: AppBar(
         //    title: Text("Welcome to Flutter App"),
         //  ),
-        body: Center(
-            // child: Text("Hello Word"),
-            // 此处替换为 StatefulWidget、State结合使用 Copy
-            // child: Text(wordPair.asPascalCasre),
-            //  child: RandomWords(),
-            // child: CountDown(title:'Count Down')
-            child: HomeWidget()),
-      ),
+         body: Center(
+           // child: Text("Hello Word"),
+           // 此处替换为 StatefulWidget、State结合使用 Copy
+           // child: Text(wordPair.asPascalCasre),
+          //  child: RandomWords(),
+          // child: CountDown(title:'Count Down')
+          child: HomeWidget()
+          ),
+         ),
       // home: RandomWords(),
     );
   }
