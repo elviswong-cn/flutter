44aa9336cc9c6f00ecddbaf496da4c0cfbe36d3f
ElvisWong <elviswong@aliyun.com>
2019-07-02 18:40:38 +0800

    init

--- flutter_trip/android/app/src/main/kotlin/com/elviswong/tour/flutter_tour/MainActivity.kt
+++ flutter_trip/android/app/src/main/kotlin/com/elviswong/tour/flutter_tour/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import org.devio.flutter.splashscreen.SplashScreen
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this,true)
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
--- flutter_trip/lib/main.dart
+++ flutter_trip/lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
 import 'package:flutter_trip/pages/tab_navigator.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      FlutterSplashScreen.hide();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
       title: 'Flutter Demo',
       routes: {
