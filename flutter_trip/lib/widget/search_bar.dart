fee1b2668a4c445d147cf40b534cd1ae3c26b246
ElvisWong <elviswong@aliyun.com>
2019-07-03 14:51:11 +0800

    add searchBar

---
+++ flutter_trip/lib/widget/search_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchBar extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
